4408a8288542416ce9d92b140f94763e36b175fe
King-web-cell-05 <kingsleydada159@gmail.com>
2026-08-04 23:25:05 +0100

    Created all pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
-
+import { Toaster } from "react-hot-toast";
+import { Routes, Route } from "react-router-dom";
+import LoginLanding from "./pages/LoginLanding";
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard";
+import Employees from "./pages/Employees";
+import Attendance from "./pages/Attendance";
+import Leave from "./pages/Leave";
+import PaySlips from "./pages/PaySlips";
+import Settings from "./pages/Settings";
+import PrintPaySlips from "./pages/PrintPayslip";
+import { Navigate } from "react-router-dom";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Toaster />
+      <Routes>
+        <Route path="/login" element={<LoginLanding />} />
+        <Route element={<Layout />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/employees" element={<Employees />} />
+          <Route path="/attendance" element={<Attendance />} />
+          <Route path="/leave" element={<Leave />} />
+          <Route path="/payslips" element={<PaySlips />} />
+          <Route path="/settings" element={<Settings />} />
+        </Route>
+        <Route path="/print/payslips" element={<PrintPaySlips />} />
+        <Route path="*" element={<Navigate to="/dashboard" replace />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
