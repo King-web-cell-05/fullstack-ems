7553def669d483effd12f54d8fbcea522d34a6a8
King-web-cell-05 <kingsleydada159@gmail.com>
2026-08-11 22:30:29 +0100

    Added loading screen on dashboard page

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { dummyAdminDashboardData, dummyEmployeeDashboardData } from "../assets/assets";
+import Loading from "../components/Loading";
 const Dashboard = () => {
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(true)
@@ -11,7 +12,7 @@ const Dashboard = () => {
     },1000)
   }, [])
 
-  if(loading) return <p>Loading</p>
+  if(loading) return <Loading/>
   if(!data) return <p className="text-center text-slate-500 py-12"> Failed to load dashboard</p>
   
   if(data.role ==="ADMIN"){
