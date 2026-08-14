511099cd9c12ea2fa74434f4277f6c513dc4a8bc
King-web-cell-05 <kingsleydada159@gmail.com>
2026-08-14 14:26:15 +0100

    Fixed an error

--- client/src/components/AdminDashboard.jsx
+++ client/src/components/AdminDashboard.jsx
@@ -1,39 +1,40 @@
-import {CalendarIcon, FileTextIcon, UsersIcon} from 'lucide-react'
-import React from 'react'
+import { CalendarIcon, FileTextIcon, UsersIcon } from "lucide-react";
+import React from "react";
 
-const AdminDashboard = ({data}) => {
-    const stats =[
-        {
-            icon: UsersIcon,
-            value: data.totalEmployees,
-            label: "Total Employees",
-            description: "Active workforce",
-        },
-         {
-            icon: CalendarIcon,
-            value: data.totalAttendance,
-            label: "Today's Attendance",
-            description: "Organization units",
-        },
-             {
-            icon: CalendarIcon,
-            value: data.totalAttendance,
-            label: "Today's Attendance",
-            description: "Checked in today",
-        },
-             {
-            icon: FileTextIcon,
-            value: data.pendingLeaves,
-            label: "Pending Leaves",
-            description: "Awaiting approval",
-        },
-    ]
+const AdminDashboard = ({ data }) => {
+  const stats = [
+    {
+      icon: UsersIcon,
+      value: data.totalEmployees,
+      label: "Total Employees",
+      description: "Active workforce",
+    },
+    {
+      icon: CalendarIcon,
+      value: data.totalAttendance,
+      label: "Today's Attendance",
+      description: "Organization units",
+    },
+    {
+      icon: CalendarIcon,
+      value: data.totalAttendance,
+      label: "Today's Attendance",
+      description: "Checked in today",
+    },
+    {
+      icon: FileTextIcon,
+      value: data.pendingLeaves,
+      label: "Pending Leaves",
+      description: "Awaiting approval",
+    },
+  ];
   return (
     <div className="animate-fade-in">
       <div className="page-header">
         <h1 className="page-title">Dashboard!</h1>
         <p className="page-subtitle">
-            Welcome back, Admin - here's your overview        </p>
+          Welcome back, Admin - here's your overview{" "}
+        </p>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-5 mb-8">
         {stats.map((s) => (
@@ -52,9 +53,8 @@ const AdminDashboard = ({data}) => {
           </div>
         ))}
       </div>
-     
     </div>
-  )
-}
+  );
+};
 
-export default AdminDashboard
+export default AdminDashboard;
