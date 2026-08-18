44a6ffc6866305ab709ed9843253e6ea64ed8cf3
King-web-cell-05 <kingsleydada159@gmail.com>
2026-08-18 23:00:52 +0100

    Refactor EmployeeCard component layout for improved styling and structure

--- client/src/components/EmployeeCard.jsx
+++ client/src/components/EmployeeCard.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 const EmployeeCard = ({employee, onDelete, onEdit}) => {
   return (
     <div className='group relative card card-hover overflow-hidden'>
-        <div></div>
+        <div className='relative aspect-4/3 w-full overflow-hidden bg-linear-to-br from-slate-100 to-slate-50'>
+            
+
+            <div className='w-full h-full flex items-center justify-center'>
+                {/* circle icons */}
+                <div className='w-20 h-20 rounded-full bg-linear-to-br from-indigo-100 to-slate-100 flex items-center justify-center'>
+                    <span>{employee.firstName[0]}{employee.lastName[0]}</span>
+                </div>
+            </div>
+        </div>
         <div className='p-5'>
             <h3>{employee.firstName} {employee.lastName}</h3>
             <p className='text-xs text-slate-500'>{employee.position}</p>
