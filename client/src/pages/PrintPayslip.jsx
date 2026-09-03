059f11b5333ce80a5afa8e8190c67bc96e8e7180
King-web-cell-05 <kingsleydada159@gmail.com>
2026-09-03 19:12:43 +0100

    Implement PrintPayslip component with data fetching and loading state management

--- client/src/pages/PrintPayslip.jsx
+++ client/src/pages/PrintPayslip.jsx
@@ -1,5 +1,36 @@
+import {useCallback, useState} from "react";
+
+
 const PrintPayslip = () => {
-  return <div>PrintPayslip</div>;
+  const [payslips, setPayslips] = useState([]);
+  const [employees, setEmployees] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const isAdmin = true;
+
+  const fetchPayslips = useCallback(async () => {
+    setPayslips(dummyPayslipData);
+    setTimeout(() => {
+      setLoading(false);
+    },1000);
+  },[]);
+
+  useEffect(() => {
+    fetchPayslips()
+  },[fetchPayslips]);
+   
+    useEffect(() => {
+    if(isAdmin) setEmployees(dummyEmployeeData);
+  },[isAdmin]);
+
+  if(loading) return <Loading/>
+
+  return <div className="animate-fade-in">
+    <div>
+      <div>
+        <h1></h1>
+      </div>
+    </div>
+  </div>;
 };
 
 export default PrintPayslip;
