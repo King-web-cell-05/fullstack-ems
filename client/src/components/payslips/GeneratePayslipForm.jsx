2295086568c8b9a6aa9834834d9a18db4b11fcf5
King-web-cell-05 <kingsleydada159@gmail.com>
2026-09-07 23:28:06 +0100

    Add modal structure and close button to GeneratePayslipForm component

--- client/src/components/payslips/GeneratePayslipForm.jsx
+++ client/src/components/payslips/GeneratePayslipForm.jsx
@@ -1,5 +1,6 @@
 import { Plus } from "lucide-react";
 import React, { useState } from "react";
+import { X } from "lucide-react";
 
 const GeneratePayslipForm = ({ employees, onSucess }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,7 +20,24 @@ const GeneratePayslipForm = ({ employees, onSucess }) => {
     e.preventDefault();
   };
 
-  return <div>GeneratePayslipForm</div>;
+  return (
+    <div className="fixed inset-0 bg-black/40  backdrop-blur-sm flex items-center justify-center z-50 p-4">
+      <div className="card max-w-lg w-full p-6 animate-slide-up">
+        <div className="flex justify-between items-center mb-6">
+          <h3 className="text-lg font-bold text-slate-900">
+            Generate Monthly Payslip
+          </h3>
+          <button
+            className="text-slate-400 hover:text-slate-600 p-1"
+            type="button"
+            onClick={() => setIsOpen(false)}
+          >
+            <X size={20} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default GeneratePayslipForm;
