bb6fffa55425ace90276bd4678ef94bd4b7b17aa
King-web-cell-05 <kingsleydada159@gmail.com>
2026-09-08 23:01:55 +0100

    Implement profile fetching and loading state in Settings component

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import Loading from "../components/Loading";
+import { dummyProfileData } from "../assets/assets";
+
 const Settings = () => {
-  return <div>Settings</div>;
+  const [profile, setProfile] = useState(null); 
+  const [loading, setLoading] = useState(true); 
+  const [showPasswordModal, setShowPasswordModal] = useState(false); 
+
+
+  const fetchProfile = async () => {
+    setProfile(dummyProfileData);
+    const timeoutId = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timeoutId);
+  }
+
+  useEffect(() => {
+    const cleanup = fetchProfile();
+
+    return () => {
+      cleanup.then((clearTimeout) => clearTimeout());
+    };
+  },[]);
+
+  if (loading) return <Loading />;
+
+
+  return <div className="animate-fade-in">
+    <div className="page-header">
+      <h1 className="page-title">Settings</h1>
+      <p className="page-subtitle">Manage your account settings and preferences</p>
+    </div>
+  </div>;
 };
 
 export default Settings;
